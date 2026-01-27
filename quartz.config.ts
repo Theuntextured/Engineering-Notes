dcc0f413998bfcfd8b6cc393772f37c120f11efc
Riccardo Giorgi <69459631+Theuntextured@users.noreply.github.com>
2026-01-27 23:02:22 +0100

    Quartz sync: Jan 27, 2026, 11:02 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "Theuntextured.github.io/Engineering-Notes",
     ignorePatterns: ["private", "Templates", ".obsidian", ".git", "node_modules"],
     defaultDateType: "modified",
     theme: {
